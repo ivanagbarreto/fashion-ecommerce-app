05a56c011bd94a75085fef170c1fcd7fd340632b
ivanagbarreto <ivanagbarreto@gmail.com>
2025-08-21 10:35:50 -0300

    icono en flatcard

--- src/screens/shop/CategoriesScreens.jsx
+++ src/screens/shop/CategoriesScreens.jsx
@@ -1,39 +1,66 @@
-import { StyleSheet, Text, View, Image, FlatList, Pressable } from 'react-native'
-import categories from '../../data/categories.json'
-import FlatCard from '../../components/FlatCard';
-
-const CategoriesScreens = ({navigation}) => {
-
-const renderCategoryItem = ({item})=>{
-    return(
-        <Pressable onPress={()=>navigation.navigate("Productos",{category: item.title})}>
-          <FlatCard>
-            <Image width= {30} height={30} source={{uri:item.image}} resizeMode='contain'/> 
-            <Text style={styles.title}> {item.title}</Text>
-          </FlatCard>
-        </Pressable>
-  )
-}
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  Pressable,
+} from "react-native";
+import categories from "../../data/categories.json";
+import FlatCard from "../../components/FlatCard";
+import Icon from "react-native-vector-icons/Feather";
+import { colors } from "../../global/colors";
+const CategoriesScreens = ({ navigation }) => {
+  const renderCategoryItem = ({ item }) => {
+    return (
+      <Pressable
+        onPress={() =>
+          navigation.navigate("Productos", { category: item.title })
+        }
+      >
+        <FlatCard style={styles.container}>
+          
+           
+              <Image
+                width={30}
+                height={30}
+                source={{ uri: item.image }}
+                resizeMode="contain"
+                style={styles.leftContent}
+              />
+              <Text style={styles.title}> {item.title}</Text>
+            
+       
+              <Icon name={item.icon} size={15} color={colors.darkGray} />
+           
+         
+        </FlatCard>
+      </Pressable>
+    );
+  };
 
   return (
     <FlatList
-          data={categories}
-          renderItem={renderCategoryItem}
-          keyExtractor={item=> item.id}
-          />
-  )
-}
+      data={categories}
+      renderItem={renderCategoryItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
+};
 
-export default CategoriesScreens
+export default CategoriesScreens;
 
-const styles = StyleSheet.create({  
-    container: {
-            flex: 1,
-            backgroundColor: '#fff',
-    
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    flexDirection:"row"
   },
-  title:{
-    fontFamily:"RobotoCondensed-Regular"
-  }
 
-})
+  title: {
+    fontFamily: "RobotoCondensed-Regular",
+  },
+  icon: {
+     marginLeft: "auto",
+  },
+});
